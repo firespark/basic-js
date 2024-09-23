29101bc254781637fb4093a9b28dc64d7b6989ce
Elena Markova <fireleit@gmail.com>
2024-09-23 16:45:47 +0800

    feat: add functions excepting vigenere cipher

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const keys = [];
+  const values = [];
+  const newArr = [];
+
+  arr.forEach( (num, key) => {
+    if (num == -1) {
+        keys.push(key);
+    }
+    else {
+        values.push(num);
+    }
+  });
+
+  const sortedValues = values.sort((a, b) => a - b);
+
+  for (var i = 0; i <= keys[keys.length - 1]; i++) {
+
+      if (keys.includes(i)) {
+
+        newArr.push(-1);
+      }
+      else {
+        newArr.push(sortedValues[0]);
+        sortedValues.shift();
+      }
+  }
+
+  return [...newArr, ...sortedValues];
+  
 }
 
 module.exports = {
