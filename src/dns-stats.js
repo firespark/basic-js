03e2028d48221fbfa12ffa414b3531feb127f688
Elena Markova <fireleit@gmail.com>
2024-09-23 12:31:44 +0800

    feat: add dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+
+  const stats = {};
+
+  domains.forEach(domain => {
+    let arr = domain.split('.').reverse();
+
+    let dns = '';
+
+    arr.forEach(el => {
+      dns += `.${el}`;
+      stats[dns] = (stats[dns] || 0) + 1;
+    });
+
+  });
+
+  return stats;
 }
 
 module.exports = {
